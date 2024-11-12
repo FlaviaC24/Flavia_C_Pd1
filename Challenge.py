599bf0fe730e3162a49107f6ca1340473f99fb37
FlaviaC24 <flaviac3@nycstudents.net>
2024-11-12 09:06:16 -0500

    Create Challenge.py

---
+++ Challenge.py
@@ -0,0 +1,10 @@
+def main():
+  print(value)
+  dice1=randit(1,6)
+  print(f"Dice1:{dice1}and Dice1:{dice2}.")
+  from random important randint
+  value=dice1+dice2 
+value=4*dice1 
+else:
+if dice1==dice2:
+main()
